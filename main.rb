72d7e3099040f7acdfdbf74314a5e7a79d9320ba
Justin Louie <224840+nitsujri@users.noreply.github.com>
2019-09-28 12:57:12 +0800

    Prog

--- main.rb
+++ main.rb
@@ -1,12 +1,38 @@
 require 'rpi_gpio'
 
-def setup
+# Pins
+RELAY_1 = 3
+RELAY_2 = 5
+PIR_PIN = 7
 
+# Globals
+
+def setup
+  RPi::GPIO.set_numbering :board
+
+  RPi::GPIO.setup PIR_PIN, as: :input
+
+  RPi::GPIO.setup RELAY_1, as: :output
+  RPi::GPIO.setup RELAY_2, as: :output
 end
 
 def loop
+  if RPi::GPIO.high? PIR_PIN
+    set_relays 'high'
+  else
+    set_relays 'low'
+  end
 
 end
 
+def set_relays(output)
+  [RELAY_1, RELAY_2].each do |relay|
+    RPi::GPIO.send("set_#{output}", relay)
+  end
+end
+
+#### Run Program ####
 setup
-while(true) { loop }
+while(true) do
+  loop
+end
